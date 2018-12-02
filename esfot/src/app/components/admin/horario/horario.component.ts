ae8b147087921e48b0b93b6b74e87588bcda05e1
Oscar Llumiquinga <davichochaos@hotmail.com>
2018-12-02 14:39:24 -0500

    cambios

--- esfot/src/app/components/admin/horario/horario.component.ts
+++ esfot/src/app/components/admin/horario/horario.component.ts
@@ -6,6 +6,7 @@ import {Horarios} from '../../../interfaces/horarios.interface'
 import {Materias} from '../../../interfaces/materias.interface';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Docentes} from '../../../interfaces/docentes.interface';
+import {tokenReference} from '@angular/compiler';
 
 @Component({
   selector: 'app-horario',
@@ -19,29 +20,28 @@ export class HorarioComponent implements OnInit {
   aulas: Aulas[] = [];
   materias: Materias[] = [];
   docentes: Docentes[] = [];
-
   horario: Horarios = {
-    dia: '',
-    dia1: '',
-    dia2: '',
-    dia3: '',
-    dia4: '',
-    dia5: '',
+    dia: null,
+    dia1: null,
+    dia2: null,
+    dia3: null,
+    dia4: null,
+    dia5: null,
     nombreMat: '',
     docenteNom: '',
     nombreAula: '',
-    horaInicio: '',
-    horaFin: '',
-    horaInicio1: '',
-    horaFin1: '',
-    horaInicio2: '',
-    horaFin2: '',
-    horaInicio3: '',
-    horaFin3: '',
-    horaInicio4: '',
-    horaFin4: '',
-    horaInicio5: '',
-    horaFin5: '',
+    horaInicio: null,
+    horaFin: null,
+    horaInicio1: null,
+    horaFin1: null,
+    horaInicio2: null,
+    horaFin2: null,
+    horaInicio3: null,
+    horaFin3: null,
+    horaInicio4: null,
+    horaFin4: null,
+    horaInicio5: null,
+    horaFin5: null,
   }
 
 
@@ -103,13 +103,28 @@ export class HorarioComponent implements OnInit {
   ngOnInit() {
   }
 
+  //rstrincciones
+  //1. Hora finalizada menor a la hora de inicio y
+  // sugunda horario empezar desde la hora finalizada de la anterior
+
   hora2() {
     //let text= (<HTMLInputElement> document.getElementById("inicio")).value.replace(':','0');
     let horaDesde = (document.getElementById("inicio") as HTMLInputElement).value.replace(':','0');
     let horaHasta = (document.getElementById("fin")as HTMLInputElement).value.replace(':','0');
     let horaDesde1 = (document.getElementById("inicio1") as HTMLInputElement).value.replace(':','0');
     let horaHasta1 = (document.getElementById("fin1")as HTMLInputElement).value.replace(':','0');
-    if (Date.parse(horaDesde) > Date.parse(horaHasta) || Date.parse(horaDesde1) > Date.parse(horaHasta1)) {
+    let horaDesde2 = (document.getElementById("inicio2") as HTMLInputElement).value.replace(':','0');
+    let horaHasta2 = (document.getElementById("fin2")as HTMLInputElement).value.replace(':','0');
+    let horaDesde3 = (document.getElementById("inicio3") as HTMLInputElement).value.replace(':','0');
+    let horaHasta3 = (document.getElementById("fin3")as HTMLInputElement).value.replace(':','0')
+    let horaDesde4 = (document.getElementById("inicio4") as HTMLInputElement).value.replace(':','0');
+    let horaHasta4 = (document.getElementById("fin4")as HTMLInputElement).value.replace(':','0');
+    let horaDesde5 = (document.getElementById("inicio5") as HTMLInputElement).value.replace(':','0');
+    let horaHasta5 = (document.getElementById("fin5")as HTMLInputElement).value.replace(':','0')
+
+    if (Date.parse(horaDesde) > Date.parse(horaHasta) || Date.parse(horaDesde1) > Date.parse(horaHasta1) ||
+        Date.parse(horaDesde2) > Date.parse(horaHasta2) || Date.parse(horaDesde3) > Date.parse(horaHasta3) ||
+        Date.parse(horaDesde4) > Date.parse(horaHasta4) || Date.parse(horaDesde5) > Date.parse(horaHasta5)) {
       this.msgs = [];
       this.msgs.push({severity:'error', summary:'Error', detail:'Hora final es inferior a hora de inicio'});
 
@@ -121,7 +136,7 @@ export class HorarioComponent implements OnInit {
     }*/
   }
 
-  mat() {
+  materiasAsig() {
     let mat = (document.getElementById("mat")as HTMLInputElement).value;
     let mat1 = (document.getElementById("mat1") as HTMLInputElement).value;
     let mat2 = (document.getElementById("mat2")as HTMLInputElement).value;
@@ -148,6 +163,116 @@ export class HorarioComponent implements OnInit {
             this._router.navigate(['/admin' ]);
           }
         );
+    }
+  }
+
+  cod: boolean = true;
+  cod1: boolean = true;
+  cod2: boolean = true;
+  cod3: boolean = true;
+  cod4: boolean = true;
+  cod5: boolean = true;
+
+// activaciones de inputs a traves de checkbox
+  val() {
+    switch (this.cod) {
+      case true:
+        this.horario.dia = 'Lunes';
+        this.cod = false;
+
+        break;
+      case false:
+        this.horario.dia = null;
+        this.cod = true;
+
+        break;
+      default:
+        console.log("hola");
+    }
+  }
+
+  val1() {
+    switch (this.cod1) {
+      case true:
+        this.horario.dia1 = 'Martes';
+        this.cod1 = false;
+
+        break;
+      case false:
+        this.horario.dia1 = null;
+        this.cod1 = true;
+
+        break;
+      default:
+        console.log("hola");
+    }
+  }
+
+  val2() {
+    switch (this.cod2) {
+      case true:
+        this.horario.dia2 = 'Miercoles';
+        this.cod2 = false;
+
+        break;
+      case false:
+        this.horario.dia2 = null;
+        this.cod2 = true;
+
+        break;
+      default:
+        console.log("hola");
+    }
+  }
+
+  val3() {
+    switch (this.cod3) {
+      case true:
+        this.horario.dia3 = 'Jueves';
+        this.cod3 = false;
+
+        break;
+      case false:
+        this.horario.dia3 = null;
+        this.cod3 = true;
+
+        break;
+      default:
+        console.log("hola");
+    }
+  }
+
+  val4() {
+    switch (this.cod4) {
+      case true:
+        this.horario.dia4 = 'Viernes';
+        this.cod4 = false;
+
+        break;
+      case false:
+        this.horario.dia4 = null;
+        this.cod4 = true;
+
+        break;
+      default:
+        console.log("hola");
+    }
+  }
+
+  val5() {
+    switch (this.cod5) {
+      case true:
+        this.horario.dia5 = 'Sabado';
+        this.cod5 = false;
+
+        break;
+      case false:
+        this.horario.dia5 = null;
+        this.cod5 = true;
+
+        break;
+      default:
+        console.log("hola");
     }
   }
 }
