45c67f6937bfc94d8345cab58ef4b9c9776d3773
davichochaos <davichochaos@hotmail.com>
2019-01-03 12:34:56 -0500

    #3 control total

--- esfot/src/app/interfaces/horarios.interface.ts
+++ esfot/src/app/interfaces/horarios.interface.ts
@@ -1,23 +1,8 @@
 export interface Horarios {
-  dia0?: string;
-  dia1?: string;
-  dia2?: string;
-  dia3?: string;
-  dia4?: string;
-  dia5?: string;
+  dias: string[];
   nombreMat: string;
   docenteNom: string;
   nombreAula: string;
-  horaInicio0?: string;
-  horaFin0?: string;
-  horaInicio1?: string;
-  horaFin1?: string;
-  horaInicio2?: string;
-  horaFin2?: string;
-  horaInicio3?: string;
-  horaFin3?: string;
-  horaInicio4?: string;
-  horaFin4?: string;
-  horaInicio5?: string;
-  horaFin5?: string;
+  horaInicios: string[];
+  horaFins: string[];
 }
